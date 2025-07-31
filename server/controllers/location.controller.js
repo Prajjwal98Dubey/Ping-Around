1194e3d19699366b75e99146c58ca0944041c10b
prajjwal98dubey <prajjwaldubey003@gmail.com>
2025-07-31 23:54:13 +0530

    Refactor getCloseByUsers to improve user location querying with distance calculation and bucketed results

--- server/controllers/location.controller.js
+++ server/controllers/location.controller.js
@@ -29,11 +29,77 @@ export const getCloseByUsers = async (req, res) => {
   const userId = req.userId;
   try {
     let obj = { 1: [], 3: [], 5: [], 10: [], 11: [] };
-    let userDetails = await pingPool.query(
-      "SELECT U1.USER_ID,U1.FIRST_NAME,U1.USER_EMAIL,U1.USER_IMAGE,U1.PROFESSION,U2.LATITUDE,U2.LONGITUDE,U3.USER_INSTAGRAM,U3.USER_TWITTER,U3.USER_LINKEDIN,U3.USER_GITHUB,U3.USER_REDDIT,U3.USER_FACEBOOK,U3.USER_RANDOM_SOCIAL FROM USERS U1 INNER JOIN LOCATION_DETAILS U2 ON U1.USER_ID = U2.USER_ID AND U2.IS_ONLINE = true AND U1.USER_ID <> $1 INNER JOIN USER_SOCIALS U3 ON U2.USER_ID = U3.USER_ID",
-      [userId]
+    let nearUserLocationDetails = await pingPool.query(
+      `
+    WITH distance_calc AS (
+  SELECT
+    user_id,
+    latitude,
+    longitude,
+    is_online,
+    (
+      6371 * acos(
+        cos(radians($1)) * cos(radians(latitude)) *
+        cos(radians(longitude) - radians($2)) +
+        sin(radians($1)) * sin(radians(latitude))
+      )
+    ) AS distance_km
+  FROM
+    LOCATION_DETAILS
+  WHERE
+    user_id != $3
+    AND is_online = TRUE 
+),
+bucketed AS (
+  SELECT
+    *,
+    FLOOR(distance_km) AS floored_distance_km,
+    CASE
+      WHEN FLOOR(distance_km) <= 1 THEN '1'
+      WHEN FLOOR(distance_km) <= 3 THEN '3'
+      WHEN FLOOR(distance_km) <= 5 THEN '5'
+      WHEN FLOOR(distance_km) <= 10 THEN '10'
+      ELSE '11'
+    END AS distance_bucket
+  FROM
+    distance_calc
+),
+ranked AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY distance_bucket ORDER BY floored_distance_km) AS rn
+  FROM
+    bucketed
+)
+SELECT
+  distance_bucket,
+  user_id,
+  latitude,
+  longitude,
+  floored_distance_km AS distance_km
+FROM
+  ranked
+WHERE
+  rn <= 10
+ORDER BY
+  distance_bucket, floored_distance_km;
+
+      `,
+      [lat, long, userId]
     );
-    for (let user of userDetails.rows) {
+    let nearUserDetails = [];
+    for (let obj of nearUserLocationDetails.rows) {
+      nearUserDetails.push(
+        // CAN USE BETTER QUERY (THINK ABOUT IT !!!)
+        pingPool.query(
+          "SELECT U1.USER_ID,U1.FIRST_NAME,U1.USER_EMAIL,U1.USER_IMAGE,U1.PROFESSION,U2.LATITUDE,U2.LONGITUDE,U3.USER_INSTAGRAM,U3.USER_TWITTER,U3.USER_LINKEDIN,U3.USER_GITHUB,U3.USER_REDDIT,U3.USER_FACEBOOK,U3.USER_RANDOM_SOCIAL FROM USERS U1 INNER JOIN LOCATION_DETAILS U2 ON U1.USER_ID = U2.USER_ID AND U1.USER_ID = $1 INNER JOIN USER_SOCIALS U3 ON U2.USER_ID = U3.USER_ID AND U2.USER_ID = $1",
+          [obj.user_id]
+        )
+      );
+    }
+    let details = await Promise.all(nearUserDetails);
+    details = details.map((detail) => detail.rows[0]);
+    for (let user of details) {
       let distance = haverSineDistance(
         { lat1: lat, lon1: long },
         { lat2: user["latitude"], lon2: user["longitude"] }

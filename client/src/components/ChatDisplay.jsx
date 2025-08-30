b53fa3140ea5bb2dfbf189961b53f937d14888fb
prajjwal98dubey <prajjwaldubey003@gmail.com>
2025-08-30 18:48:50 +0530

    chat display UI fix !!!

--- client/src/components/ChatDisplay.jsx
+++ client/src/components/ChatDisplay.jsx
@@ -1,6 +1,8 @@
 import { use, useEffect, useRef, useState } from "react";
 import { CacheColorContext, UserContext } from "../context/all.context";
 import { FaCamera } from "react-icons/fa";
+import { LuSendHorizontal } from "react-icons/lu";
+
 import { HANDLE_USER_IMAGE_UPLOAD } from "../apis/auth.api";
 
 const ChatDisplay = ({ chats, socketRef, roomId, setChats }) => {
@@ -13,7 +15,8 @@ const ChatDisplay = ({ chats, socketRef, roomId, setChats }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [fileDetails, setFileDetails] = useState({});
   const [selectedImage, setSelectedImage] = useState("");
-
+  const [isImageZoom, setIsImageZoom] = useState(false);
+  const [zoomImageUrl, setZoomImageUrl] = useState("");
   useEffect(() => {
     const setScrollHeight = () => {
       scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
@@ -87,35 +90,39 @@ const ChatDisplay = ({ chats, socketRef, roomId, setChats }) => {
     >
       <div
         ref={scrollRef}
-        className="w-full h-[90%]  bg-[#313131] my-2 rounded-md overflow-auto"
+        className="w-full h-[90%] bg-[#313131] my-2 rounded-md overflow-auto py-2"
       >
         {chats.map((chat, index) => {
           return chat.me ? (
-            <div key={index} className="w-full flex px-1 justify-end my-1">
+            <div key={index} className="w-full flex px-2 justify-end my-2">
               {chat.isImage ? (
                 <div className="px-2 py-2">
                   <img
+                    onClick={() => {
+                      setZoomImageUrl(chat.imageUrl);
+                      setIsImageZoom(true);
+                    }}
                     src={chat.imageUrl}
                     alt="photo"
-                    className="w-[60px] h-[60px] rounded-md"
+                    className="w-[200px] h-[150px] rounded-md"
                   />
                 </div>
               ) : (
-                <div className="px-2 bg-green-500 font-bold w-fit h-fit rounded-md">
-                  <p className=" text-white text-[13px] font-bold my-[2px]">
+                <div className="px-2 bg-green-600 font-bold w-fit h-fit py-2 rounded-md">
+                  <p className=" text-white text-[14px] font-bold my-[2px]">
                     {chat.message}
                   </p>
                 </div>
               )}
             </div>
           ) : (
-            <div key={index} className="w-full flex px-1 justify-start my-1">
-              <div className="px-1 flex justify-center items-center">
+            <div key={index} className="w-full flex px-1 justify-start my-2">
+              <div className="flex justify-center items-center px-1">
                 {chat.userImage ? (
                   <img
                     src={chat.userImage}
                     alt="loading"
-                    className="w-[20px] h-[20px] rounded-full"
+                    className="w-[30px] h-[30px] rounded-full"
                   />
                 ) : (
                   <div
@@ -129,23 +136,29 @@ const ChatDisplay = ({ chats, socketRef, roomId, setChats }) => {
                   </div>
                 )}
               </div>
-              <div className="px-2 bg-amber-500 font-bold w-fit h-fit rounded-md">
+              <div className="px-2 bg-purple-500 font-bold w-fit h-fit rounded-md py-2">
                 <div className="flex justify-start">
-                  <p className="text-purple-700 text-[11px] font-semibold">
+                  <p className="text-amber-300 text-[11px] font-semibold">
                     {chat.userName}
                   </p>
                 </div>
                 {chat.isImage ? (
-                  <div className="px-2 py-2">
+                  <div
+                    onClick={() => {
+                      setZoomImageUrl(chat.imageUrl);
+                      setIsImageZoom(true);
+                    }}
+                    className=""
+                  >
                     <img
                       src={chat.imageUrl}
                       alt="photo"
-                      className="w-[60px] h-[60px] rounded-md"
+                      className="w-[200px] h-[150px] rounded-md"
                     />
                   </div>
                 ) : (
                   <div>
-                    <p className=" text-white text-[13px] font-bold mb-[3px]">
+                    <p className=" text-white text-[14px] font-bold mb-[3px]">
                       {chat.message}
                     </p>
                   </div>
@@ -156,28 +169,12 @@ const ChatDisplay = ({ chats, socketRef, roomId, setChats }) => {
         })}
       </div>
       <div className="w-full h-[10%] relative">
-        <div className=" absolute left-1 top-[10px] hover:border hover:border-gray-600 cursor-pointer border border-gray-400 bg-[#313131] rounded-full w-[40px] h-[40px] px-1 py-1 flex justify-center items-center text-white font-bold">
-          <label>
-            <FaCamera className="cursor-pointer" />
-            <input
-              onChange={(e) => {
-                setFileName(e.target.files[0].name);
-                setFileType(e.target.files[0].type);
-                setFileDetails(e.target.files[0]);
-                setSelectedImage(e.target.files[0]);
-              }}
-              type="file"
-              className="sr-only"
-              accept="image/*"
-            />
-          </label>
-        </div>
         <textarea
           id="inp_container"
           value={message}
           rows="1"
           onChange={(e) => setMessage(e.target.value)}
-          className="w-full h-full bg-[#313131] rounded-md text-white font-bold text-xl px-2 py-1"
+          className="w-full h-full bg-[#313131] rounded-md text-white font-bold text-[16px] pr-[90px] pl-2 py-1"
         ></textarea>
         {fileName && (
           <div className="fixed top-0 left-3">
@@ -188,15 +185,50 @@ const ChatDisplay = ({ chats, socketRef, roomId, setChats }) => {
             />
           </div>
         )}
-        <button
-          onClick={handleSendMessage}
-          className={`absolute right-0 top-0 ${
-            isLoading ? "bg-slate-400" : "bg-blue-500"
-          } h-full w-[130px] rounded-md text-xl font-bold text-white`}
+        <div
+          className={`absolute right-0 top-3 h-fit w-fit px-4 rounded-md text-xl font-bold text-white flex`}
         >
-          send
-        </button>
+          <div className="border border-gray-600 px-2 py-2 rounded-[10px] mx-1 hover:border-gray-400 text-gray-200 text-lg flex justify-center items-center">
+            <label>
+              <FaCamera className="cursor-pointer" />
+              <input
+                onChange={(e) => {
+                  setFileName(e.target.files[0].name);
+                  setFileType(e.target.files[0].type);
+                  setFileDetails(e.target.files[0]);
+                  setSelectedImage(e.target.files[0]);
+                }}
+                type="file"
+                className="sr-only"
+                accept="image/*"
+              />
+            </label>
+          </div>
+          <div className="flex justify-center items-center">
+            <button
+              onClick={handleSendMessage}
+              className="border border-gray-600 px-2 py-2 rounded-[10px] hover:border-gray-400 text-gray-200 text-lg"
+            >
+              <LuSendHorizontal />
+            </button>
+          </div>
+        </div>
       </div>
+      {isImageZoom && (
+        <div
+          onClick={() => setIsImageZoom(false)}
+          className="transition duration-300 fixed top-0 left-0 bg-black bg-opacity-50 backdrop-blur-sm z-10 w-full min-h-screen"
+        >
+          <div className="transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 fixed">
+            <img
+              onClick={(e) => e.stopPropagation()}
+              src={zoomImageUrl}
+              alt="image_zoom"
+              className="w-[400px] h-[300px] rounded-md z-50 blur-none"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
